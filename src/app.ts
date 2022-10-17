aff708719c25005dcca33e196d77f8436736c42f
daniil <daniil_baldin@ukr.net>
2022-10-17 11:55:58 +0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -70,6 +70,12 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/', adminRouter, photoRouter, albumRouter);
 
 app.listen(PORT, () => {
