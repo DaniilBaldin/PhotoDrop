b00b3f9f3193fe8c07cd72cefec8fc77f891a139
daniil <daniil_baldin@ukr.net>
2022-10-17 17:03:26 +0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -63,8 +63,8 @@ app.use(express.static(path.join(__dirname, '../', '/public')));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, UPDATE, HEAD, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, origin, content-type, accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
