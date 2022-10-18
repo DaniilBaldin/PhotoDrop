542fd319fbe01254a3fea4b444823e2174faee57
daniil <daniil_baldin@ukr.net>
2022-10-18 09:44:43 +0300

    Fix

--- src/controllers/albums/updateAlbum.ts
+++ src/controllers/albums/updateAlbum.ts
@@ -16,12 +16,12 @@ const updateAlbum = async (req: InfoRequest, res: Response) => {
             res.status(404).json({ message: 'Album not found!' });
         } else {
             // console.log(updateAlbum);
-            const album_logo = req.body.album_logo ?? updateAlbum.album_logo;
+            const album_logo = req.body.album_logo || updateAlbum.album_logo || 'default';
             const album_name = req.body.album_name ?? updateAlbum.album_name;
             const album_location = req.body.album_location ?? updateAlbum.album_location;
             const date = new Date(req.body.date).toISOString();
             console.log(album_logo, album_location, album_name, date);
-            Albums.updateAlbum(album_logo, album_name, album_location, date, id, person_id);
+            await Albums.updateAlbum(album_logo, album_name, album_location, date, id, person_id);
             res.status(200).json({ message: 'Updated!' });
         }
     } catch (err) {
