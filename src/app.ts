faac9d5b4f3055a77335fade3ea222154432f667
daniil <daniil_baldin@ukr.net>
2022-11-03 12:51:05 +0200

    Fix

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../', '/Public')));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setTimeout(1500);
+    res.setTimeout(2000);
     next();
 });
 
